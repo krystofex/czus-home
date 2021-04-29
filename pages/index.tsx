00dcd07430d078f08de524c86468243d7c6225eb
Kryštof <48121710+krystofex@users.noreply.github.com>
2021-04-29 02:02:44 +0200

    react-grid-layout implementation start

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import 'tailwindcss/tailwind.css';
 import React, { useState, useEffect } from 'react';
 import Head from 'next/head';
 import Widget from '../src/components/widgets/WidgetController';
-
+import GridLayout from 'react-grid-layout';
 import settings from '../src/components/settings/czusHomeConfig.json';
 
 const MainGrid = (props) => {
@@ -30,18 +30,64 @@ const Home = () => {
 
     return (
         <>
-            <Head>
-                <title>{settings.rooms[0].name}</title>
-                <link rel="icon" href="icons/favicon.ico" />
-            </Head>
-            <MainGrid>
-                {widgetArray}{' '}
-                <Widget
-                    widgetName={'controlPanel'}
-                    name={''}
-                    position={[6, 0, 2, 1]}
-                />
-            </MainGrid>
+            <GridLayout
+                className="layout"
+                cols={12}
+                rowHeight={30}
+                width={1200}
+            >
+                <div
+                    className="rounded-widget shadow-custom p-2 bg-light-widget dark:bg-dark-widget"
+                    key="b"
+                    data-grid={{
+                        x: 1,
+                        y: 3,
+                        w: 3,
+                        h: 2,
+                        minW: 2,
+                        maxW: 4,
+                        isResizable: true,
+                        isDraggable: true,
+                        esizeHandles: true,
+                    }}
+                >
+                    b
+                </div>
+                <div
+                    className="rounded-widget shadow-custom p-2 bg-light-widget dark:bg-dark-widget"
+                    key="c"
+                    data-grid={{
+                        x: 1,
+                        y: 1,
+                        w: 3,
+                        h: 2,
+                        minW: 2,
+                        maxW: 4,
+                        isResizable: true,
+                        isDraggable: true,
+                        esizeHandles: true,
+                    }}
+                >
+                    b
+                </div>
+                <div
+                    className="rounded-widget shadow-custom p-2 bg-light-widget dark:bg-dark-widget"
+                    key="d"
+                    data-grid={{
+                        x: 1,
+                        y: 2,
+                        w: 3,
+                        h: 2,
+                        minW: 2,
+                        maxW: 4,
+                        isResizable: true,
+                        isDraggable: true,
+                        esizeHandles: true,
+                    }}
+                >
+                    b
+                </div>
+            </GridLayout>
         </>
     );
 };
